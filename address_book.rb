8e760fe69b6efc9454a09d907684c5847add70ff
Jeff Austin <jeffaustin81@gmail.com>
2016-01-09 02:29:31 -0800

    Add search method

--- address_book.rb
+++ address_book.rb
@@ -7,6 +7,24 @@ class AddressBook
     @contacts = []
   end
 
+  def find_by_name(name)
+    results = []
+    search = name.downcase
+    contacts.each do |contact|
+      if contact.first_name.downcase.include?(search) ||
+         contact.last_name.downcase.include?(search)
+        results.push(contact)
+      end
+    end
+    puts "Name search results (#{search})"
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
+  end
+
   def print_contact_list
     puts "Contact List"
     contacts.each do |contact|
